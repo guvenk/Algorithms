37dff5de19307475db2b98557f21b33adb569a6f
Guven <guven89@hotmail.com>
2020-05-31 16:58:11 +0200

    Add Extensions.cs, PagedList.cs and StringHelper.cs

---
+++ Algorithms/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+
+
+namespace Algorithms
+{
+    public static class Extensions
+    {
+        // using System.Text.Json;
+        public static T Parse<T>(this HttpResponseMessage response)
+        {
+            string content = response.Content?.ReadAsStringAsync().Result;
+            return System.Text.Json.JsonSerializer.Deserialize<T>(content);
+        }
+
+        // using Newtonsoft.Json;
+        public static T Parse2<T>(this HttpResponseMessage response)
+        {
+            string content = response.Content?.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+    }
+}
---
+++ Algorithms/PagedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    // Used for paging
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalCount { get; private set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PagedList(List<T> items, int totalCount, int pageNumber, int pageSize)
+        {
+            TotalCount = totalCount;
+            CurrentPage = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            AddRange(items);
+        }
+
+        public static PagedList<T> Create(IQueryable<T> list, int pageNumber, int pageSize)
+        {
+            var total = list.Count();
+            var items = list.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, total, pageNumber, pageSize);
+        }
+    }
+}
