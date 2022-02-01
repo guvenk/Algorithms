c841a034e303afc6369fbb8027498d96c4b08bd5
Guven Sezgin Kurt <guven89@hotmail.com>
2022-02-01 01:19:57 +0100

    Dijkstra's algorithm

---
+++ Algorithms/Graph/Dijkstra.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public class Dijkstra
+    {
+        // find shortest distance to other nodes from node k
+        // n is number of nodes
+        // times[0] = node A
+        // times[1] = node B
+        // times[2] = Distance
+        public int NetworkDelayTime(int[][] times, int n, int k)
+        {
+            var graph = new Dictionary<int, List<int[]>>();
+            foreach (var edge in times)
+            {
+                if (!graph.ContainsKey(edge[0]))
+                    graph.Add(edge[0], new List<int[]>());
+                graph[edge[0]].Add(new int[] { edge[1], edge[2] });
+            }
+            var heap = new SortedList<int, int>(new CustomComparer());
+            heap.Add(0, k);
+
+            var dist = new Dictionary<int, int>();
+
+            while (heap.Count > 0)
+            {
+                (int d, int node) = heap.First();
+                heap.RemoveAt(0);
+
+                if (dist.ContainsKey(node))
+                    continue;
+
+                dist.Add(node, d);
+                if (graph.ContainsKey(node))
+                {
+                    foreach (var edge in graph[node])
+                    {
+                        int neighbor = edge[0];
+                        int d2 = edge[1];
+                        if (!dist.ContainsKey(neighbor))
+                            heap.Add(d + d2, neighbor);
+                    }
+                }
+            }
+
+            if (dist.Count != n)
+                return -1;
+
+            return dist.Values.Max();
+        }
+
+        public class CustomComparer : IComparer<int>
+        {
+            public int Compare(int x, int y) => x < y ? -1 : 1;
+        }
+    }
+}
--- Algorithms/Solution.cs
+++ Algorithms/Solution.cs
@@ -14,87 +14,17 @@ namespace Algorithms
 
 
 
-            var points = new int[5][];
-            points[0] = new int[2] { 0, 0 };
-            points[1] = new int[2] { 2, 2 };
-            points[2] = new int[2] { 3, 10 };
-            points[3] = new int[2] { 5, 2 };
-            points[4] = new int[2] { 7, 0 };
-            var res = s.MinCostConnectPoints(points);
-            Console.WriteLine(res);
+            var points = new int[3][];
+            points[0] = new int[3] { 2, 1, 1 };
+            points[1] = new int[3] { 2, 3, 1 };
+            points[2] = new int[3] { 3, 4, 1 };
+
+            //var res = s.NetworkDelayTime(points, 4, 2);
+            //Console.WriteLine(res);
 
             Console.ReadKey();
         }
-        public int NetworkDelayTime(int[][] times, int n, int k)
-        {
-            graph = new Dictionary<int, IList<(int Target, int Cost)>>();
-            dist = new Dictionary<int, int>();
 
-        }
-        Dictionary<int, IList<(int Target, int Cost)>> graph;
-        Dictionary<int, int> dist;
-
-        public int MinCostConnectPoints(int[][] points)
-        {
-            int total = 0;
-            List<int[]> list = new();
-
-            root = new int[points.Length];
-            rank = new int[points.Length];
-
-            for (int i = 0; i < root.Length; i++)
-            {
-                root[i] = i;
-                rank[i] = 1;
-            }
-
-            for (int i = 0; i < points.Length - 1; i++)
-                for (int j = i + 1; j < points.Length; j++)
-                    list.Add(new int[] { i, j, Math.Abs(points[i][0] - points[j][0]) + Math.Abs(points[i][1] - points[j][1]) });
-
-            list.Sort((a, b) => a[2].CompareTo(b[2]));
-
-            foreach (var item in list)
-                if (Find(item[0]) != Find(item[1]))
-                {
-                    total += item[2];
-                    Union(item[0], item[1]);
-                }
-
-            return total;
-        }
-        private int[] root = null;
-        private int[] rank = null;
-        private void Union(int x, int y)
-        {
-            int px = Find(x),
-                py = Find(y);
-
-            if (px != py)
-            {
-                if (rank[px] > rank[py])
-                {
-                    root[py] = px;
-                }
-                else if (rank[py] > rank[px])
-                {
-                    root[px] = py;
-                }
-                else
-                {
-                    root[py] = px;
-                    rank[px]++;
-                }
-            }
-        }
-
-        private int Find(int x)
-        {
-            if (x != root[x])
-                root[x] = Find(root[x]);
-
-            return root[x];
-        }
 
 
         public class FreqStack
