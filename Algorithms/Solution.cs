18634bec0b9f720ca003b3bc1b40f2f5a6760d7d
Guven Sezgin Kurt <guven89@hotmail.com>
2022-02-09 21:12:53 +0100

    Update

--- Algorithms/Solution.cs
+++ Algorithms/Solution.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Algorithms
@@ -12,58 +10,19 @@ namespace Algorithms
         {
             var s = new Solution();
 
+            // var edges = new int[3][];
+            // edges[0] = new int[2] { 0, 1 };
+            // edges[1] = new int[2] { 1, 2 };
+            // edges[2] = new int[2] { 2, 0 };
+            var graph = new int[4][];
+            graph[0] = new int[] { 0, 1 };
+            graph[1] = new int[] { 0, 2 };
+            graph[2] = new int[] { 1, 3 };
+            graph[3] = new int[] { 2, 3 };
+            // edges[4] = new int[2] { 4, 3 };
 
 
-            var points = new int[3][];
-            points[0] = new int[3] { 2, 1, 1 };
-            points[1] = new int[3] { 2, 3, 1 };
-            points[2] = new int[3] { 3, 4, 1 };
-
-            //var res = s.NetworkDelayTime(points, 4, 2);
-            //Console.WriteLine(res);
-
-            Console.ReadKey();
         }
 
-
-
-        public class FreqStack
-        {
-            readonly Dictionary<int, int> _freq;
-            readonly Dictionary<int, Stack<int>> _group;
-            int _maxFreq;
-
-            public FreqStack()
-            {
-                _freq = new Dictionary<int, int>();
-                _group = new Dictionary<int, Stack<int>>();
-            }
-
-            public void Push(int val)
-            {
-                if (!_freq.ContainsKey(val))
-                    _freq.Add(val, 0);
-
-                var freq = ++_freq[val];
-                _maxFreq = Math.Max(_maxFreq, freq);
-
-                if (!_group.ContainsKey(freq))
-                    _group.Add(freq, new Stack<int>());
-
-                _group[freq].Push(val);
-            }
-
-            public int Pop()
-            {
-                int x = _group[_maxFreq].Pop();
-                _freq[x]--;
-
-                if (_group[_maxFreq].Count == 0)
-                    _maxFreq--;
-
-                return x;
-            }
-        }
     }
 }
-
